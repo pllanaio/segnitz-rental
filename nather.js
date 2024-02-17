ee05d3289fc3d51c92d4e5c7a0206baa8b8ba4ca
Leon <leon.pllana@asc-vision.de>
2024-02-17 12:10:07 +0100

    Added REST-API for further form data handling

--- nather.js
+++ nather.js
@@ -1,11 +1,37 @@
 const express = require("express");
+const path = require('path');
+const fs = require('fs');
 const app = express();
+
+app.use(express.json());
 app.use(express.static("public"));
 
+const filePath = path.join(__dirname, 'public', 'formdata', 'formData.json');
+
+function saveDataToFile(data) {
+  fs.writeFileSync(filePath, data, 'utf8');
+}
+
+app.post('/data', (req, res) => {
+  const data = JSON.stringify(req.body, null, 2);
+  saveDataToFile(data);
+  res.send({ message: 'Daten erfolgreich gespeichert.' });
+});
+
+app.get('/data', (req, res) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Fehler beim Lesen der Datei:', err);
+      return res.status(500).send('Fehler beim Lesen der Datei');
+    }
+    res.send(data);
+  });
+});
+
 app.get('/', (req, res) => {
-  res.sendFile('index.html');
-})
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(3000, function() {
-  console.log("Nather Auftragserfassung by ASC Vision");
+  console.log("Server läuft auf Port 3000");
 });
