d2a826806a5f091139bf8f1193cdeaafcf887c51
Leon <leon.pllana@asc-vision.de>
2024-04-19 13:35:26 +0200

    Only enabling one checkbox activated in step 1, Improved the UI in step 1 for having the 3 buttons alligned to the rest,

--- public/js/checkstate.js
+++ public/js/checkstate.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded', function () {
     var DisposeTextfieldElement = document.getElementById('DisposeTextfield');
     var WorkCheckElement = document.getElementById('WorkCheck');
     var WorkTextfield = document.getElementById('WorkTextfield');
+    var RenterCheck = document.getElementById('Renter');
+    var OwnerCheck = document.getElementById('Owner');
+    var other_relatedCheck = document.getElementById('other_related');
 
     MachineUsageCheckElement.addEventListener('change', function () {
         MachineUsageTextfieldElement.disabled = !MachineUsageCheckElement.checked;
@@ -17,4 +20,33 @@ document.addEventListener('DOMContentLoaded', function () {
     WorkCheckElement.addEventListener('change', function () {
         WorkTextfield.disabled = !WorkCheckElement.checked;
     });
+
+
+    // Event listener for Renter checkbox
+    RenterCheck.addEventListener('change', function () {
+        if (RenterCheck.checked) {
+            // Disable other checkboxes
+            OwnerCheck.checked = false;
+            other_relatedCheck.checked = false;
+        }
+    });
+    
+    // Event listener for Owner checkbox
+    OwnerCheck.addEventListener('change', function () {
+        if (OwnerCheck.checked) {
+            // Disable other checkboxes
+            RenterCheck.checked = false;
+            other_relatedCheck.checked = false;
+        }
+    });
+    
+    // Event listener for other_related checkbox
+    other_relatedCheck.addEventListener('change', function () {
+         if (other_relatedCheck.checked) {
+            // Disable other checkboxes
+            RenterCheck.checked = false;
+            OwnerCheck.checked = false;
+        }
+    });
+
 });
